fa086da8997331fd7327296379b5acce20232495
Yazeed Zakarneh <yazeed.zaka@gmail.com>
2025-11-02 11:07:06 +0200

    Add View ALL Products method

--- Inventory.cs
+++ Inventory.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace ISM
 {
-    internal class Inventory
+    public class inventory
     {
-    }
+        private List<Product> products;
+
+        public inventory()
+        {
+            products = new List<Product>();
+        }
+
+
+        public void AddProduct(string name, decimal price, int quantity)
+        {
+            products.Add(new Product(name, price, quantity));
+        }
+
+        public string ViewAllProducts()
+        {
+            string allProducts = string.Empty;
+            foreach (Product product in products)
+            {
+                allProducts += $"Product Name: {product.Name}, Price: {product.Price}, Quantity: {product.Quantity} /n";
+            }
+
+            return allProducts;
+        }
+
+    };
 }
